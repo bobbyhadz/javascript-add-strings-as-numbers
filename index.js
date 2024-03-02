db6a5b34119f1d916ab5e0831f637552ad8ad4be
bobbyhadz <opmftw@gmail.com>
2024-03-02 19:32:35 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// EXAMPLE 1 - Add Strings as Numbers in JavaScript
+
+const str1 = '5';
+const str2 = '15';
+
+const result = Number(str1) + Number(str2);
+console.log(result); // 👉️ 20
+
+console.log(typeof result); // 👉️ number
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Add Strings as Numbers using the unary plus (+)
+
+// const str1 = '5';
+// const str2 = '15';
+
+// const result = +str1 + +str2;
+
+// console.log(result); // 👉️ 20
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 3 - Using `parseInt()` and `parseFloat()` to add strings as Numbers
+
+// const str1 = '5a';
+// const str2 = '15b';
+
+// const result = parseInt(str1, 10) + parseInt(str2, 10);
+
+// console.log(result); // 👉️ 20
